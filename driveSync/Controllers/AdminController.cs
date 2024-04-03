d640a38a5e66700db62d888c3e3a390a143edf80
abcool20999 <abcool20999@gmail.com>
2024-04-03 00:23:27 -0400

    admin

--- driveSync/Controllers/AdminController.cs
+++ driveSync/Controllers/AdminController.cs
@@ -3,11 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Http;
+using System.Diagnostics;
+using driveSync.Models;
+using System.Web.Script.Serialization;
+using System.Net.NetworkInformation;
+using System.Data.Entity.Migrations.Model;
 
 namespace driveSync.Controllers
 {
     public class AdminController : Controller
     {
+        private static readonly HttpClient client;
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+
+        static AdminController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44332/api/AdminData");
+        }
+
+        public ActionResult AdminLoginSubmit(Admin admin)
+        {
+            Debug.WriteLine(admin.username);
+            Debug.WriteLine(admin.password);
+
+            string url = "Validate";
+            string jsonpayload = jss.Serialize(admin);
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+
+            try
+            {
+                HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+                //if (response.IsSuccessStatusCode)
+                {
+                    Admin resUser = response.Content.ReadAsAsync<Admin>().Result;
+
+                    //Session.Clear();
+                    //Session["userId"] = resUser;
+                    //var action = $"PassengerProfile";
+                    //return RedirectToAction(action, "Passenger");
+
+                    return RedirectToAction("AdminProfile", "Admin");
+
+                }
+                //else
+                //{
+                //    Debug.WriteLine("Unsuccessful login attempt.");
+                //    return RedirectToAction("Index", "Home"); // Redirect to home page if login fails
+                //}
+            }
+            catch (Exception ex)
+            {
+                // Log the exception details
+                Debug.WriteLine("An error occurred during login: " + ex.Message);
+                // Redirect to an error page or handle the error appropriately
+                return RedirectToAction("Error", "Admin");
+            }
+        }
+
+        public ActionResult AdminProfile()
+        {
+
+
+            // Pass user object to the view
+            return View();
+        }
         // GET: Admin
         public ActionResult Index()
         {
